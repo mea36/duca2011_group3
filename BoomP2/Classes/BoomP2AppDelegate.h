2698fd4eeca87377c0f1464e300748fde32b8a6c
Jake Yamaki <jyamaki@ducomputingacademy.org>
2011-07-19 14:12:19 -0400

    Project Start

---
+++ BoomP2/Classes/BoomP2AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  BoomP2AppDelegate.h
+//  BoomP2
+//
+//  Created by CS Admin on 7/19/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface BoomP2AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Untitled/Classes/UntitledAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  UntitledAppDelegate.h
+//  Untitled
+//
+//  Created by CS Admin on 7/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UntitledAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
+
