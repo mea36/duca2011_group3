b675f96fdf33e9357154daa9c9e90760b64b2ff3
Geordan Lane <gxlane@gmail.com>
2011-07-19 15:54:39 -0400

    startscreen stsrt

--- BoomP2/Classes/BoomP2AppDelegate.h
+++ BoomP2/Classes/BoomP2AppDelegate.h
@@ -11,7 +11,7 @@
 @class RootViewController;
 
 @interface BoomP2AppDelegate : NSObject <UIApplicationDelegate> {
-	UIWindow			*window;
+	UIWindow			*startscreen;
 	RootViewController	*viewController;
 }
 
